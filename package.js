f3cf5da0db6e99d7611f50b540c73c5028076ecf
Cooper Knaak <cooperknaak@gmail.com>
2017-05-08 23:39:30 -0500

    Initial commit.

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'easy-modals',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.4.2');
+  api.use('ecmascript');
+  api.mainModule('easy-modals.js');
+  api.use('peppelg:bootstrap-3-modal');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('easy-modals');
+  api.mainModule('easy-modals-tests.js');
+});
